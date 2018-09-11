df3d41fb410fa35d9bdad0f0f5a56f81969c7c8d
Jon Perkins <captainperk81@gmail.com>
2018-09-11 18:16:48 -0400

    Prepaing the Views

--- ControllersAndActions/Startup.cs
+++ ControllersAndActions/Startup.cs
@@ -6,8 +6,6 @@ namespace ControllersAndActions
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -15,7 +13,6 @@ namespace ControllersAndActions
             services.AddSession();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseStatusCodePages();
