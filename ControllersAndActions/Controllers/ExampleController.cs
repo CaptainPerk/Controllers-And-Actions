9569471b5e370bf112a16c883d29d1c84bf0d49e
Jon Perkins <captainperk81@gmail.com>
2018-10-21 12:47:12 -0400

    Passing Data from an Action Method to a View

---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index() => View(DateTime.Now);
+
+        public ViewResult Result() => View(model:"Hello World");
+    }
+}
---
+++ ControllersAndActions_Tests/Controllers/ExampleControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using ControllersAndActions.Controllers;
+using System;
+using Xunit;
+
+namespace ControllersAndActions_Tests.Controllers
+{
+    public class ExampleControllerTests
+    {
+        [Fact(DisplayName = "The model of the Index view is a DateTime")]
+        public void CorrectModelTypeISUsed()
+        {
+            // Arrange
+            var exampleController = new ExampleController();
+
+            // Act
+            var viewResult = exampleController.Index();
+
+            // Assert
+            Assert.IsType<DateTime>(viewResult.ViewData.Model);
+        }
+    }
+}
